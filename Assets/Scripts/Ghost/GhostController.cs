c8ee995243814a847d3936552bf12b35c5fd77ca
Damien-Joseph RISPAL <damienjoseph.rispal@gmail.com>
2022-09-11 20:09:40 -0400

    Ghost animation

--- Assets/Scripts/Ghost/GhostController.cs
+++ Assets/Scripts/Ghost/GhostController.cs
@@ -7,15 +7,35 @@ public class GhostController : MonoBehaviour
     public Vector2 initialDirection;
     [SerializeField] private float speed;
     public Vector2 newDirection;
+    private GhostAnimationController _animationController;
+    public SpriteRenderer EyeSpriteRenderer;
+    [SerializeField] private Sprite[] _sprites;
 
     private void Start()
     {
         newDirection = initialDirection;
+        _animationController = GetComponent<GhostAnimationController>();
+        _animationController.PlayRun();
     }
 
     private void Update()
     {
         transform.position += (new Vector3(newDirection.x, newDirection.y)*speed*Time.deltaTime);
-
+        if(newDirection == Vector2.up)
+        {
+            EyeSpriteRenderer.sprite = _sprites[0];
+        } 
+        else if(newDirection == Vector2.right)
+        {
+            EyeSpriteRenderer.sprite = _sprites[1];
+        }
+        else if (newDirection == Vector2.down)
+        {
+            EyeSpriteRenderer.sprite = _sprites[2];
+        }
+        else if (newDirection == Vector2.left)
+        {
+            EyeSpriteRenderer.sprite = _sprites[3];
+        }
     }
 }
